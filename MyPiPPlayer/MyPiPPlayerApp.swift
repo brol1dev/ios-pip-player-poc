a029f52253d4c08026f22494af376986aa31f7b5
brol1dev <eric.vargas.mx@gmail.com>
2025-07-16 12:24:45 -0500

    Initial Commit

---
+++ MyPiPPlayer/MyPiPPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPiPPlayerApp.swift
+//  MyPiPPlayer
+//
+//  Created by Eric on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MyPiPPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
